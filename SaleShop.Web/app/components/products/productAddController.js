d2fb49ad6d2cf3260ba706a27bfcfed6853523f3
Lukas Nguyen <Dat.NguyenThaiThanh@studentpartner.com>
2017-09-13 00:24:52 +0700

    Manage multi images

--- SaleShop.Web/app/components/products/productAddController.js
+++ SaleShop.Web/app/components/products/productAddController.js
@@ -7,19 +7,25 @@
 
     function productAddController(apiService, $scope, notificationService, $state, commonService) {
 
+        //Xử lý ckeditor
         $scope.ckeditorOptions = {
             language: 'vi',
             height: '200px'
         };
 
+        //Chọn hình ảnh
         $scope.ChooseImage = function(){
             var finder = new CKFinder();
             finder.selectActionFunction = function (fileUrl) {
-                $scope.product.Image = fileUrl;
+                //Thêm $scope.$apply bắt angular thực hiện ngay function này
+                $scope.$apply(function () {
+                    $scope.product.Image = fileUrl;
+                });
             }
             finder.popup();
         }
 
+        //Tạo auto make SEO tiltle
         $scope.GetSeoTitle = GetSeoTitle;
 
         function GetSeoTitle() {
@@ -36,6 +42,9 @@
         $scope.AddProduct = AddProduct;
 
         function AddProduct() {
+
+            $scope.product.MoreImages = JSON.stringify($scope.moreImages);
+
             apiService.post('/api/product/create', $scope.product, (result) => {
                 notificationService.displaySuccess(result.data.Name + ' đã được thêm mới');
                 $state.go('products');
@@ -53,6 +62,22 @@
         }
 
         loadProductCategories();
+
+        //Xử lý more images
+        $scope.moreImages = [];
+        $scope.ChooseMoreImage = ChooseMoreImage;
+        function ChooseMoreImage() {
+            var finder = new CKFinder();
+            finder.selectActionFunction = function (fileUrl) {
+
+                //Thêm $scope.$apply bắt angular thực hiện ngay function này
+                $scope.$apply(function() {
+                    $scope.moreImages.push(fileUrl);
+                });
+            }
+            finder.popup();
+        }
+
 
     }
 
--- SaleShop.Web/app/components/products/productEditController.js
+++ SaleShop.Web/app/components/products/productEditController.js
@@ -7,10 +7,45 @@
 
     function productEditController(apiService, $scope, notificationService, $state, commonService, $stateParams) {
 
+
+
+        //Biến load nhiều hình ảnh
+        $scope.moreImages = [];
+        //Xử lý more images
+        $scope.ChooseMoreImage = ChooseMoreImage;
+        function ChooseMoreImage() {
+            var finder = new CKFinder();
+            finder.selectActionFunction = function (fileUrl) {
+
+                //Thêm $scope.$apply bắt angular thực hiện ngay function này
+                $scope.$apply(function () {
+                    $scope.moreImages.push(fileUrl);
+                });
+            }
+            finder.popup();
+        }
+        //Bỏ chọn tất cả ảnh
+        $scope.DeleteAllSelectedImage = DeleteAllSelectedImage;
+        function DeleteAllSelectedImage() {
+            $scope.moreImages = [];
+        }
+        //Bỏ chọn ảnh được chọn
+        $scope.UnselectedImage = UnselectedImage;
+        function UnselectedImage(img) {
+            console.log(Object.values(img));
+            console.log($scope.moreImages);
+            var imgSelected = Object.values(img)[0];
+            console.log($scope.moreImages.indexOf(imgSelected));
+            $scope.moreImages.splice($scope.moreImages.indexOf(imgSelected), 1);
+            //  $scope.moreImages = $scope.moreImages.filter(function (e) { return e !== img })
+        }
+
         $scope.loadProductDetail = loadProductDetail;
         function loadProductDetail() {
             apiService.get('/api/product/getbyid/' + $stateParams.id, null, (result) => {
                 $scope.product = result.data;
+
+                $scope.moreImages = JSON.parse($scope.product.MoreImages);
             }, (error) => {
                 notificationService.displayError(error.data);
             });
@@ -24,7 +59,10 @@
         $scope.ChooseImage = function () {
             var finder = new CKFinder();
             finder.selectActionFunction = function (fileUrl) {
-                $scope.product.Image = fileUrl;
+                //Thêm $scope.$apply bắt angular thực hiện ngay function này
+                $scope.$apply(function () {
+                    $scope.product.Image = fileUrl;
+                });
             }
             finder.popup();
         }
@@ -44,6 +82,8 @@
         $scope.UpdateProduct = UpdateProduct;
 
         function UpdateProduct() {
+            $scope.product.MoreImages = JSON.stringify($scope.moreImages);
+
             apiService.put('/api/product/update', $scope.product, (result) => {
                 notificationService.displaySuccess(result.data.Name + ' đã được cập nhật');
                 $state.go('products');

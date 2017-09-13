b9c876861ee4bb4ac41b42cbf8a0e1cfc3123b3d
Lukas Nguyen <Dat.NguyenThaiThanh@studentpartner.com>
2017-09-13 18:48:36 +0700

    Update Login Form and using nested template

--- SaleShop.Web/app/components/products/products.module.js
+++ SaleShop.Web/app/components/products/products.module.js
@@ -8,16 +8,19 @@
             {
                 url: '/products',
                 templateUrl: '/app/components/products/productListView.html',
+                parent:'base',
                 controller: 'productListController'
             }).state('product_add',
             {
                 url: '/product_add',
                 templateUrl: '/app/components/products/productAddView.html',
+                parent: 'base',
                 controller: 'productAddController'
             }).state('product_edit',
             {
                 url: '/product_edit/:id',
                 templateUrl: '/app/components/products/productEditView.html',
+                parent: 'base',
                 controller: 'productEditController'
             });
     }
